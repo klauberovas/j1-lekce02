d1fc6b208da194c595ce122fde1614192d10e1cc
Šárka Klauberová <sarkaa.novakova@seznam.cz>
2025-04-08 20:30:14 +0200

    create methods for drawing house: drawSquare a drawRoof

--- src/main/java/cz/czechitas/java/lekce02/HlavniProgram.java
+++ src/main/java/cz/czechitas/java/lekce02/HlavniProgram.java
@@ -13,6 +13,35 @@ public class HlavniProgram {
     zofka = new Turtle();
 
     //TODO tady bude kód
+    for (int i = 0; i < 4 ; i++) {
+      drawHouse();
+      startAgain();
+    }
   }
 
+  public void drawSquare(){
+    for (int i = 0; i < 4 ; i++) {
+      zofka.turnLeft(90);
+      zofka.move(100);
+    }
+  }
+  public void drawRoof(){
+    zofka.turnLeft(30);
+    zofka.move(100);
+    zofka.turnLeft(120);
+    zofka.move(100);
+  }
+
+  public void drawHouse(){
+    drawSquare();
+    drawRoof();
+  }
+
+  public void startAgain(){
+    zofka.penUp();
+    zofka.turnLeft(120);
+    zofka.move(250);
+    zofka.turnLeft(90);
+    zofka.penDown();
+  }
 }
