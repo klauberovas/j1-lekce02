defd7acf8d5e313e8fd2cf34d4782303ef781887
Šárka Klauberová <sarkaa.novakova@seznam.cz>
2025-04-13 12:03:58 +0200

    create logic for drawing pig

--- src/main/java/cz/czechitas/java/lekce02/HlavniProgram.java
+++ src/main/java/cz/czechitas/java/lekce02/HlavniProgram.java
@@ -13,35 +13,72 @@ public class HlavniProgram {
     zofka = new Turtle();
 
     //TODO tady bude kód
-    for (int i = 0; i < 4 ; i++) {
-      drawHouse();
-      startAgain();
-    }
-  }
-
-  public void drawSquare(){
-    for (int i = 0; i < 4 ; i++) {
-      zofka.turnLeft(90);
-      zofka.move(100);
-    }
-  }
-  public void drawRoof(){
+    //upper line
+    zofka.turnRight(90);
+    zofka.move(200);
+    //right line
+    zofka.turnRight(90);
+    zofka.move(100);
+    //back legs
     zofka.turnLeft(30);
-    zofka.move(100);
-    zofka.turnLeft(120);
-    zofka.move(100);
-  }
-
-  public void drawHouse(){
-    drawSquare();
-    drawRoof();
-  }
-
-  public void startAgain(){
+    zofka.move(40);
     zofka.penUp();
-    zofka.turnLeft(120);
-    zofka.move(250);
-    zofka.turnLeft(90);
+    zofka.turnRight(120);
+    zofka.move(40);
+    zofka.turnRight(120);
     zofka.penDown();
+    zofka.move(40);
+    //lower line
+    zofka.turnLeft(120);
+    zofka.move(200);
+    //front legs
+    zofka.turnLeft(120);
+    zofka.move(40);
+    zofka.penUp();
+    zofka.turnRight(120);
+    zofka.move(40);
+    zofka.turnRight(120);
+    zofka.penDown();
+    zofka.move(40);
+    //head
+    zofka.turnLeft(90);
+    zofka.move(100);
+    zofka.turnRight(120);
+    zofka.move(100);
+    //left line
+    zofka.turnRight(120);
+    zofka.move(100);
+
+//    for (int i = 0; i < 4 ; i++) {
+//      drawHouse();
+//      startAgain();
+//    }
   }
+
+
+//  public void drawSquare(){
+//    for (int i = 0; i < 4 ; i++) {
+//      zofka.turnLeft(90);
+//      zofka.move(100);
+//    }
+//  }
+//  public void drawRoof(){
+//    zofka.turnLeft(30);
+//    zofka.move(100);
+//    zofka.turnLeft(120);
+//    zofka.move(100);
+//  }
+
+//  public void drawHouse(){
+//    drawSquare();
+//    drawRoof();
+//  }
+//
+//  public void startAgain(){
+//    zofka.penUp();
+//    zofka.turnLeft(120);
+//    zofka.move(250);
+//    zofka.turnLeft(90);
+//    zofka.penDown();
+//  }
 }
